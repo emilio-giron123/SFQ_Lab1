e49a800ab88836b815bbeba3d462f2064104040f
emilio-giron123 <103013731+emilio-giron123@users.noreply.github.com>
2025-01-28 21:16:24 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,44 @@
+package com.ontariotechu.sofe3980U;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	* Main program:  The entry point of the program. The local time will be printed first,
+	*      Then it will create two binary variables, add them and print the result.
+	*
+	* @param args: not used
+	*/
+    public static void main( String[] args )
+    {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		Binary binary1=new Binary("00010001000");
+        System.out.println( "First binary number is "+binary1.getValue());
+		Binary binary2=new Binary("111000");
+        System.out.println( "Second binary number is "+binary2.getValue());
+		Binary sum= Binary.add(binary1,binary2);
+		System.out.println( "Their summation is "+sum.getValue());
+
+		//Computes the OR operation and outputs it
+		Binary or = Binary.or(binary1, binary2);
+		System.out.println("\nAfter bitwise OR operation: "+or.getValue());
+
+
+
+		//Computes the AND operation and outputs it
+		Binary and = Binary.and(binary1, binary2);
+		System.out.println("\nAfter bitwise AND operation: "+and.getValue());
+
+
+		//Computes the MULTIPLY operation and outputs it
+		Binary multiply = Binary.multiply(binary1, binary2);
+		System.out.println("\nMultiplying them gives: "+multiply.getValue());
+
+	}
+}
